3ac2b06318d8c792ea1b5a88b59bf38fa62d831a
Alex Savchenko <a.savchenko@codetiburon.com>
2014-05-05 18:32:52 +0300

    common config

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
-requirejs.config({
-    baseUrl: 'js/lib',
-    paths: {
-        app: '../app',
-        jquery: 'jquery/dist/jquery'
-    }
+require(['./config'], function (config) {
+    require(['app/somemodule']);
 });
-
 // Start the main app logic.
-requirejs(['jquery', 'underscore/underscore', 'backbone/backbone'],
-    function ($, _, backbone) {
-        console.log('Load!!');
-    }
-);
+//requirejs(['jquery', 'underscore/underscore', 'backbone/backbone'],
+//    function ($, _, backbone) {
+//        console.log('Load!!');
+//    }
+//);
